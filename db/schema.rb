39caecb758740d54b69a614718c4110e15ae4c82
Jordan Watson <jordanwatson915@gmail.com>
2025-07-24 11:19:49 -0700

    manually generate multiple migration tables for practice.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_24_173513) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_24_181150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 end
