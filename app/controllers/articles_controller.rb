15b2559a16f391c40d4a3fb51788477ad54d0ed6
Jordan Watson <jordanwatson915@gmail.com>
2025-07-24 14:30:04 -0700

    add ability to create new article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,15 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def new
+
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        # redirect_to article_path(@article) # redirect to that article page
+        redirect_to @article
+    end
+
 end
