7e46604b8f37dd2c97a59e5def18b1f53cd588bc
Jordan Watson <jordanwatson915@gmail.com>
2025-07-25 10:36:42 -0700

    Implement new, edit and update actions.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,14 +10,32 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
+    end
 
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
-        # redirect_to article_path(@article) # redirect to that article page
-        redirect_to @article
+        @article = Article.new(params.require(:article).permit(:title, :description))  # White list title and description
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            # redirect_to article_path(@article) # redirect to that article page
+            redirect_to @article
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
 
 end
